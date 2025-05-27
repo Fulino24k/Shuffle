070b7fa71c9c2134e7e80fbafe86cb7b7fbc3167
Fulino24k <liusean24@outlook.com>
2025-05-26 22:04:47 -0400

    vite file update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Shuffle',
+  base: '/Shuffle/',
   server: {
     host: '0.0.0.0'
   }
